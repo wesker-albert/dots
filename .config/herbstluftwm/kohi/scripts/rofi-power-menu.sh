e1850c22f8efd4d456fc973d9545a805dd428451
wesker-albert <github@umbrellacorp.io>
2023-05-04 14:33:02 -0700

    rofi-power-menu

---
+++ .config/herbstluftwm/kohi/scripts/rofi-power-menu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rofi -show power-menu \
+    -modi power-menu:'~/.config/rofi/kohi/rofi-power-menu.sh' \
+    -theme-str 'window {children: [ listview ]; width: 300px;} listview {lines: 6;}'
