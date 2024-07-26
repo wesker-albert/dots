804904116a993812f007b56b73789a89a99faac1
wesker-albert <github@umbrellacorp.io>
2024-07-26 10:28:51 -0700

    simplify updates script

--- .config/polybar/kohi/updates.sh
+++ .config/polybar/kohi/updates.sh
@@ -3,28 +3,13 @@
 set -eu -o pipefail
 
 _get_list_of_upgrades() {
-    LC_ALL=en_US apt-get -o 'Debug::NoLocking=true' --trivial-only -V dist-upgrade 2>/dev/null |
-        sed -n '/upgraded:/,$p' |
-        grep ^'  ' |
-        awk '{ print $1 }' |
-        sort
+    apt list --upgradable 2>/dev/null |
+        grep -e "upgradable from" |
+        awk -F/ '{print $1}'
 }
 
 get_update_count() {
-    UPDATE_COUNT=0
-
-    if [ -s /var/lib/synaptic/preferences ]; then
-        UPDATE_COUNT=$(_get_list_of_upgrades |
-            grep -vx $(grep 'Package:' /var/lib/synaptic/preferences 2>/dev/null | awk {'print "-e " $2'}) |
-            wc -l)
-    else
-        UPDATE_COUNT=$(_get_list_of_upgrades |
-            wc -l)
-    fi
-
-    if [ "$UPDATE_COUNT" -eq "0" ]; then
-        exit 1
-    fi
+    UPDATE_COUNT=$(_get_list_of_upgrades | wc -l)
 
     echo "$UPDATE_COUNT"
 }
