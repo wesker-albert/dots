c6655912022a597bc6cda4ced04a397c196b0a19
wesker-albert <github@umbrellacorp.io>
2024-08-19 15:51:53 -0700

    add ariang

--- .config/aria2/on-download-complete.sh
+++ .config/aria2/on-download-complete.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-dunstify \
-    --icon='document-save' \
-    'Download Completed' "$(basename "$3")"
+set -eu -o pipefail
 
+MSG_MAX_LENGTH=60
+ELIPSES="…"
+
+MSG=$(basename "$3" |
+    awk -v len="$MSG_MAX_LENGTH" \
+        '{ if (length($0) > len) print substr($0, 1, len) "'"$ELIPSES"'"; else print; }')
+
+dunstify \
+    --icon="document-save" \
+    "Aria2: Download Complete" "$MSG"
+
+mv "$3" "$HOME/Downloads"
 rm "$3.aria2"
